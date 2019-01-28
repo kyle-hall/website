e116be6050c56a16c6eea0e76cfdbf0d7cda14b0
Kyle Hall <hallkd47@gmail.com>
2019-01-28 06:43:03 -0600

    move global styles to static directory, add mdx

--- next.config.js
+++ next.config.js
@@ -1,2 +1,9 @@
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+const withMDX = require('@zeit/next-mdx')()
+module.exports = withMDX({
+  pageExtensions: ['js', 'mdx'],
+  exportPathMap: function(defaultPathMap) {
+    const pathMap = Object.assign({}, defaultPathMap)
+    delete pathMap['/index']
+    return pathMap
+  }
+})
