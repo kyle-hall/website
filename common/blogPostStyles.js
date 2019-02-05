45d1ce6cbe4195ce19cca04ec54204b4f37fc8fb
Kyle Hall <hallkd47@gmail.com>
2019-02-05 15:35:25 -0600

    refactor styles for blog posts

---
+++ common/blogPostStyles.js
@@ -0,0 +1,39 @@
+const headerStyles = {
+  fontFamily: ["Montserrat", "sans-serif"],
+  marginTop: "1em",
+  paddingTop: "1em"
+}
+
+const contentStyle = {
+  fontFamily: ["Sarabun", "sans-serif"]
+}
+
+const containerStyle = {
+  lineHeight: 1.5,
+  color: "#333",
+  paddingBottom: "1em"
+}
+
+const codeSampleStyling = {
+  backgroundColor: "#EEE",
+  paddingLeft: 15,
+  paddingTop: 20,
+  paddingBottom: 20,
+  fontFamily: ["Courier New", "sans-serif"],
+  fontSize: 14
+}
+
+const insetStyle = {
+  backgroundColor: "#EEE",
+  padding: 2,
+  fontFamily: ["Courier New", "sans-serif"],
+  borderRadius: 3
+}
+
+export default {
+  headerStyles,
+  containerStyle,
+  contentStyle,
+  codeSampleStyling,
+  insetStyle
+}
