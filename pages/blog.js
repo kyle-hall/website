ddbc199d1642e4bc3be57f1321b87f19d2564b29
Kyle Hall <hallkd47@gmail.com>
2019-04-15 20:49:02 -0500

    start of the react hooks article

--- pages/blog.js
+++ pages/blog.js
@@ -7,25 +7,30 @@ import ArticleListItem from '../components/ArticleListItem'
 const BlogIndex = props => (
   <div>
     <Head>
-      <link href='https://fonts.googleapis.com/css?family=Montserrat|Sarabun' rel='stylesheet' />
+      <link href="https://fonts.googleapis.com/css?family=Montserrat|Sarabun" rel="stylesheet" />
     </Head>
     <BlogLayout>
       <h1 style={headerStyle}>Posts</h1>
       <div style={articleListing}>
         <ArticleListItem
-          title='Deploying Static Sites with Next.js and Netlify'
-          date='2019-02-25'
-          link='/posts/deploy-static-site-with-next-and-netlify'
+          title="Test-Driving React Hooks"
+          date="2019-04-02"
+          link="/posts/test-driving-react-hooks"
         />
         <ArticleListItem
-          title='Gradle Test Output and Coverage Tools'
-          date='2019-02-05'
-          link='/posts/gradle-show-test-output'
+          title="Deploying Static Sites with Next.js and Netlify"
+          date="2019-02-25"
+          link="/posts/deploy-static-site-with-next-and-netlify"
         />
         <ArticleListItem
-          title='TDD a BST with Kotlin'
-          date='2019-01-27'
-          link='/posts/tdd-bst-with-kotlin-part-one'
+          title="Gradle Test Output and Coverage Tools"
+          date="2019-02-05"
+          link="/posts/gradle-show-test-output"
+        />
+        <ArticleListItem
+          title="TDD a BST with Kotlin"
+          date="2019-01-27"
+          link="/posts/tdd-bst-with-kotlin-part-one"
         />
       </div>
     </BlogLayout>
