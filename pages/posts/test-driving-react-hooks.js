986b7dff1ef98e8f7bd1d840d23b73f8daa63a04
Kyle Hall <hallkd47@gmail.com>
2019-05-28 21:35:22 -0500

    finished the tdd hooks article

--- pages/posts/test-driving-react-hooks.js
+++ pages/posts/test-driving-react-hooks.js
@@ -67,11 +67,168 @@ const Post = props => (
           </p>
         </section>
         <section>
+          <h2 style={blogStyles.headerStyles}>Getting Set Up</h2>
+          <p style={blogStyles.contentStyle}>
+            Assuming you have a new React project, we just need to add a couple things to get going.
+            First, check your
+            <span style={blogStyles.insetStyle}>package.json</span> file to ensure you have at least
+            version <span style={blogStyles.insetStyle}>16.8.0</span> of React, as that is the first
+            version containing hooks in the stable build. Next head to your terminal and add the
+            dependencies for react-testing-library:
+          </p>
+          <h3 style={blogStyles.contentStyle}>Using npm</h3>
+          <pre
+            style={blogStyles.codeSampleStyling}
+          >{`> npm install --save-dev react-testing-library`}</pre>
+          <h3 style={blogStyles.contentStyle}>Using yarn</h3>
+          <pre style={blogStyles.codeSampleStyling}>{`> yarn add --dev react-testing-library`}</pre>
+
+          <p style={blogStyles.contentStyle}>
+            Now, I would suggest you read the docs for yourself, such as they are in their current
+            state. I had some difficulty understanding what I need for testing this hook. The
+            relevant links are the following:
+            <ul style={blogStyles.contentStyle}>
+              <li>
+                <a href="https://github.com/testing-library/react-testing-library">
+                  The GitHub repo
+                </a>
+              </li>
+              <li>
+                <a href="https://testing-library.com/https://testing-library.com/">
+                  The home of the docs
+                </a>
+              </li>
+              <li>
+                <a href="https://testing-library.com/docs/react-testing-library/intro">
+                  The docs for react-testing-library
+                </a>
+              </li>
+            </ul>
+          </p>
+        </section>
+        <section>
           <h2 style={blogStyles.headerStyles}>Driving the Hook In</h2>
           <p style={blogStyles.contentStyle}>
-            Before getting too far, I'd like to start with a quick primer on testing React. I plan
-            to write a much bigger article about this, but an introduction to the main players
-            should be sufficient for today.
+            Great, so with that done, we can drive in this hook. The hook we'll be using, as I
+            mentioned before will be the <span style={blogStyles.insetStyle}>useState</span> hook.
+            And we'll be making a simple counter with a button to increment it. Nothing fancy for
+            this; using the testing library is the important part here. And so, we begin with a test
+            in a file called <span style={blogStyles.insetStyle}>HookCounter.spec.js</span>:
+          </p>
+          <pre style={blogStyles.codeSampleStyling}>
+            {`import { render, getByTestId, fireEvent } from 'react-testing-library'
+
+import HookCounter from './HookCounter'
+
+describe('HookCounter', () => {
+  it('should increment the counter', () => {
+    const { container } = render(<HookCounter />)
+
+    const increment = getByTestId(container, 'increment')
+
+    fireEvent.click(increment)
+
+    expect(getByTestId(container, 'count-display').textContent).toBe('1')
+  })
+})
+`}
+          </pre>
+          <p style={blogStyles.contentStyle}>
+            There's a lot going on there, so let's take a closer look. First, we import the things
+            we need from react-testing-library. We get a function to render the component, a
+            function to find the button we'll be using to increment the counter, and, finally, a way
+            to simulate a click on that button. Then we use that render function inside our test -
+            designated by the <span style={blogStyles.insetStyle}>it</span> - and deconstruct the
+            rendered container out of the object that render function produces. Next, we find our
+            button by the test ID we'll set up later and fire off a fake click event on it. That
+            should simulate the behavior we want, namely that clicking the button increments the
+            counter. Finally, we use Jest's assertion{' '}
+            <span style={blogStyles.insetStyle}>expect</span> to assert the content of the count
+            display has been changed from 0 - the initial value we assume will be used by the
+            component - to 1.
+          </p>
+          <p style={blogStyles.contentStyle}>Cool, so we run the test and that should fail.</p>
+          <p style={blogStyles.contentStyle}>
+            And it's a simple failure. We haven't created the{' '}
+            <span style={blogStyles.insetStyle}>HookCounter</span> component yet. That's easy to
+            fix:
+          </p>
+          <pre style={blogStyles.codeSampleStyling}>
+            {`import React from 'react'
+            
+export default const HookCounter = () => <div></div>`}
+          </pre>
+          <p style={blogStyles.contentStyle}>
+            That will give us enough to get through to the failure to find our button. That's a good
+            start. The empty div we had before won't cut it, anymore. Let's add some content:
+          </p>
+          <pre style={blogStyles.codeSampleStyling}>
+            {`import React from 'react'
+            
+export default const HookCounter = () => (
+  <div>
+    <h3 data-testid={'count-display}>0</h3>
+    <button
+      data-testid={'increment'}
+      type="button"
+      style={{width: 260, height: 50, backgroundColor: '#39CCCC'}}
+    >+
+    </button>
+  </div>
+)`}
+          </pre>
+          <p style={blogStyles.contentStyle}>
+            Perfect, that gets us to the assertion failure. I worked ahead a little, adding the
+            count display and doing some minor styling so we could get to that assertion failure
+            faster.
+          </p>
+          <p style={blogStyles.contentStyle}>
+            Now, the assertion says still have 0 for the display. Why's that? Well, we haven't
+            started using the hook, yet! So, let's get that in there. To do that, we need to import
+            the <span style={blogStyles.insetStyle}>useState</span> hook and create a click handler:
+          </p>
+          <pre style={blogStyles.codeSampleStyling}>
+            {`import React, { useState } from 'react'
+            
+export default const HookCounter = () => {
+  const { state: { count }, incrementCount } = useState({ count: 0 })
+  
+  return (
+    <div>
+      <h3 data-testid={'count-display}>{count}</h3>
+      <button
+        data-testid={'increment'}
+        type="button"
+        style={{width: 260, height: 50, backgroundColor: '#39CCCC'}}
+        onClick={() => incrementCount({count: count + 1})}
+      >+
+      </button>
+    </div>
+  )
+}`}
+          </pre>
+
+          <p style={blogStyles.contentStyle}>
+            And that should do it. We should have a passing test now. Notice that instead of naming
+            my deconstructed values from useState <span style={blogStyles.insetStyle}>state</span>
+            and <span style={blogStyles.insetStyle}>setState</span>, I changed their names as I
+            deconstructed them to make more sense in the app.
+          </p>
+        </section>
+        <section>
+          <h2 style={blogStyles.headerStyles}>Conclusion</h2>
+          <p style={blogStyles.contentStyle}>
+            I know this was a pretty simple example, but I want to make the business logic simple in
+            order to focus on the testing aspects. The code I originally wrote to figure this out
+            can be found in this{' '}
+            <a href="https://github.com/kyle-hall/react-hooks-with-typescript">repo</a>. I was
+            working on more than just one thing there - including TypeScript - but I liked what I
+            ended up with, and maybe the steps of the commits will be helpful.
+          </p>
+          <p style={blogStyles.contentStyle}>
+            If you have any comments or suggestions - or maybe just liked the article :) - please
+            feel free to get in touch with me via Twitter using the link below. That's all for
+            today. Thanks for reading, and I'll see you next time.
           </p>
         </section>
       </article>
