c106ff06e0615a2508393d192004b1871ed3acd6
Kyle Hall <khall@pillartechnology.com>
2019-05-29 15:13:06 -0500

    start the article

--- pages/posts/intro-to-functional-programing.js
+++
@@ -1,8 +0,0 @@
-import PostLayout from '../../components/layouts/PostLayout'
-
-const Post = props => (
-  <PostLayout title={'Introduction to Functional Programming'}>
-    <header />
-    <section />
-  </PostLayout>
-)
---
+++ pages/posts/intro-to-functional-programming.js
@@ -0,0 +1,15 @@
+import PostLayout from '../../components/layouts/PostLayout'
+
+const Post = props => (
+  <PostLayout title={'Introduction to Functional Programming'}>
+    <header>
+      <p>This is the introduction to the article</p>
+    </header>
+    <section>
+      <h2>This is the first section of the article</h2>
+      <p>This is some content in the first section of the article</p>
+    </section>
+  </PostLayout>
+)
+
+export default Post
