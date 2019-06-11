2353519eaa7ab7fc12dd771a4046e7772538aa0b
Kyle Hall <khall@pillartechnology.com>
2019-06-11 10:06:23 -0500

    move navbar into post layout

--- components/PostLayout.js
+++ components/PostLayout.js
@@ -2,12 +2,15 @@ import React from 'react'
 import BlogLayout from './BlogLayout'
 
 const PostLayout = props => (
-  <BlogLayout>
-    <div className='post-container'>
-      <h1 className='post-container__h1--big'>{props.title}</h1>
-      <article>{props.children}</article>
-    </div>
-  </BlogLayout>
+  <div>
+    <NavBar />
+    <BlogLayout>
+      <div className='post-container'>
+        <h1 className='post-container__h1--big'>{props.title}</h1>
+        <article>{props.children}</article>
+      </div>
+    </BlogLayout>
+  </div>
 )
 
 export default PostLayout
