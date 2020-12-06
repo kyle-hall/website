674c06d450aed66bb56d45c10ffb641a4e728717
Kyle Hall <hallkd47@gmail.com>
2020-12-06 17:17:39 -0600

    add filter to get three latest posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const day = require('dayjs')
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/sass')
   eleventyConfig.addPassthroughCopy('./src/css/')
   eleventyConfig.addPassthroughCopy({ 'src/images': 'assets/img' })
+
+  eleventyConfig.addFilter('latestPosts', (arr) => {
+    return arr.sort((a, b) => day(b.data.date).diff(a.data.date)).slice(0, 3)
+  })
 
   return {
     dir: {
